0dc53007b73639d29f95b161d78c34285f594ef3
Xavier Timbeau <xavier.timbeau@sciencespo.fr>
2025-05-09 18:26:27 +0200

    update

--- check wid.r
+++ check wid.r
@@ -9,27 +9,49 @@ curl::curl_download("https://wid.world/bulk_download/wid_all_data.zip",
 unzip("/tmp/wid/wid.zip", exdir = "/tmp/wid")
 dpercentile <- str_c(str_c("p", 0:99),  str_c("p", 1:100))
 
+apercentile <- c("p0p50", "p0p90", "p90p100")
+
 fr <-  vroom::vroom(glue("/tmp/wid/WID_data_FR.csv")) |>
     filter(
-      variable== "aptincj992"|
-        variable== "adiincj992"|
-        variable== "tptincj992"|
-        variable== "tdiincj992",
-      percentile %in% c("p0p50", "p50p100", "p0p90", "p0p100", dpercentile)) |>
-  filter(year==2019)
+      variable |> str_detect("ptinc")) |>
+  filter(year==2019) |>
+  mutate(pp = str_remove(percentile, "^p")) |>
+  separate(pp, into = c("pp1", "pp2"), sep = "p") |>
+  mutate(
+    pp1 = as.numeric(pp1),
+    pp2 = as.numeric(pp2),
+    type = str_sub(variable, 1,1),
+    variable = str_sub(variable, 2, 6)) |>
+ pivot_wider(names_from = type, values_from = value)
 
 fr_detail  <-  fr |>
-  filter(percentile%in%dpercentile)
+  filter(percentile %in% dpercentile) |>
+  select(variable, pp1, pp2, a, t, s) |>
+  group_by(variable) |>
+  arrange(variable, pp1, pp2) |>
+  mutate(
+    cums = cumsum(s),
+    cuma = cumsum(a)/(pp2),
+    ratio = a/s / mean(a[s>0]/s[s>0])) |>
+  ungroup()
 
 fr_aggr <-  fr |>
-  filter(!percentile%in%dpercentile)
+  filter(percentile%in%apercentile) |>
+  select(variable, percentile, year, a,s,b,t)
+
 
 fr_da <- fr_detail |>
   group_by(variable) |>
-  summarize(p0p50 = mean(value[value<=quantile(value, 0.5)]),
-            p0p90 = mean(value[value<=quantile(value, 0.9)]),
-            p0p100= mean(value)) |>
-  pivot_longer(c(p0p50, p0p90, p0p100), names_to = "percentile") |>
-  pivot_wider(names_from = variable)
+  summarize(p0p50 = mean(a[a<=quantile(a, 0.5)]),
+            p0p90 = mean(a[a<=quantile(a, 0.9)]),
+            p0p100= mean(a)) |>
+  pivot_longer(c(p0p50, p0p90, p0p100), names_to = "percentile", values_to = "a")
+
+fr_dt <- fr_detail |>
+  group_by(variable) |>
+  summarize(p0p50 = max(a[a<=quantile(a, 0.5)]),
+            p0p90 = max(a[a<=quantile(a, 0.9)]),
+            p0p100= max(a)) |>
+  pivot_longer(c(p0p50, p0p90, p0p100), names_to = "percentile", values_to = "t")
 
 fr_da
--- wid.r
+++ wid.r
@@ -2,17 +2,19 @@ library(tidyverse)
 library(glue)
 library(MetricsWeighted)
 library(ofce)
-
+library(ggrepel)
+library(ggiraph)
 dir.create("/tmp/wid")
 curl::curl_download("https://wid.world/bulk_download/wid_all_data.zip",
                     destfile = "/tmp/wid/wid.zip")
 unzip("/tmp/wid/wid.zip", exdir = "/tmp/wid")
 
-eurozone <- eurostat::ea_countries |> pull(code)
+eurozone <- eurostat::ea_countries |> pull(code) |> str_replace("EL","GR")
 
 pays <- c(eurozone, "US", "GB")
 euz <- eurozone
 dpercentile <- str_c(str_c("p", 0:99),  str_c("p", 1:100))
+
 raw <- map_dfr(pays, ~{
   vroom::vroom(glue("/tmp/wid/WID_data_{.x}.csv")) |>
     filter(
@@ -69,16 +71,6 @@ data.euz <- raw |>
   pivot_wider(names_from = variable, values_from = value.ppp) |>
   mutate(country = "EUZ")
 
-check <- raw |>
-  filter(variable %in% c("aptincj992", "adiincj992"),
-         percentile %in% c("p0p50", "p0p100"),
-         year>=1980) |>
-  select(variable, country, year, percentile, value) |>
-  left_join(ppp, by= "country") |>
-  mutate(value.ppp = value/ppp) |>
-  pivot_wider(names_from = variable, values_from = value.ppp)
-
-
 data.oth2 <- raw |>
   filter(variable %in% c("aptincj992", "adiincj992"),
          percentile %in% dpercentile,
@@ -117,37 +109,59 @@ data.oth <- raw |>
 
 dina <- bind_rows(data.euz, data.oth2)
 
-dina2 <- bind_rows(data.euz, data.oth)
-
-ggplot(dina2 |> filter(percentile %in% c("p0p50", "p0p100"))) +
-  geom_line(aes(x=year, y=adiincj992, color = percentile))+
-  scale_y_log10() +
-  facet_wrap(vars(country)) +
-  theme_ofce()
-data <- dina |>
-  filter(country%in%c("US", "GB", "FR", "EUZ"),
-         percentile %in% c("p0p50", "p50p90", "p90p100")) |>
-  pivot_longer(cols = c(adiincj992, aptincj992), names_to = "variable", values_to = "eurppp")
-
 ggplot(dina |> filter(percentile %in% c("p0p50", "p0p100"))) +
   geom_line(aes(x=year, y=adiincj992, color = percentile))+
   scale_y_log10() +
   facet_wrap(vars(country)) +
   theme_ofce()
-data <- dina |>
-  filter(country%in%c("US", "GB", "FR", "EUZ"),
-         percentile %in% c("p0p50", "p50p90", "p90p100")) |>
-  pivot_longer(cols = c(adiincj992, aptincj992), names_to = "variable", values_to = "eurppp")
 
+data <- dina |>
+  filter(country%in%c("US", "EUZ"),
+         percentile %in% c("p0p50", "p50p90", "p90p100")) |>
+  pivot_longer(cols = c(adiincj992, aptincj992), names_to = "variable", values_to = "eurppp") |>
+  group_by(year, percentile, variable) |>
+  mutate(usrel = eurppp/eurppp[country=="US"]) |>
+  group_by(percentile, country, variable) |>
+  mutate(
+    label = ifelse(year==max(year), str_c(country, " ", round(usrel*100, 0),"%"), NA_character_)
+  ) |>
+  ungroup() |>
+  mutate(
+    clabel = countrycode::countrycode(country, "iso2c", "country.name.fr"),
+    vlabel = case_match(variable,
+                        "aptincj992" ~ "Revenu pré-taxation, par individu",
+                        "adiincj992" ~ "Revenu ajusté après taxe et redistribution, par individu"),
+    variable= factor(variable, c("aptincj992", "adiincj992")),
+    tooltip = glue("<b>{clabel}</b><br>{year}<br>{vlabel} : {round(eurppp/1000,1)} k€(2023, ppp)/an/adulte")
+  )
+
+labels <- c(p0p50 = "50% les moins riches",
+            p50p90 = "entre les 50% les moins riches et\n les 10% les plus riches",
+            p90p100 = "10% les plus riches",
+            "adiincj992" = "Revenu ajusté, par individu",
+            "aptincj992" = "Revenu pré-taxation, par individu")
 ggplot(data) +
-  geom_line(aes(x=year, y=eurppp, color = country, linetype = percentile),
-            linewidth = 0.75)+
+  aes(x=year, y=eurppp, color = country) +
+  geom_line(linewidth = 0.5)+
+  ggrepel::geom_text_repel(aes(label=label), size= 2.5,
+                           nudge_x = 1,  na.rm=TRUE,
+                           max.overlaps = Inf,
+                           min.segment.length=0.5,
+                           segment.size = 0.1,
+                           xlim = c(2023, NA),
+                           direction = "y"
+                           ) +
+  geom_point_interactive(
+    aes( tooltip = tooltip, data_id = country, fill = country),
+    shape=21, color="white", stroke = 0.1, size=0.75  ) +
   facet_grid(
     cols = vars(percentile),
-    rows = vars(variable))+
+    rows = vars(variable), labeller = as_labeller(labels) )+
   scale_y_log10(
     labels = scales::label_number(scale=1/1000, suffix = "k"),
     breaks = c(20000, 30000, 40000, 50000, 100000, 150000, 200000),
     guide  = guide_axis_logticks()) +
-  PrettyCols::scale_color_pretty_d("Joyful") +
+  ofce::scale_color_pays("iso2c") +
+  coord_cartesian(expand = TRUE, clip="off") +
+  guides(color = "none", fill = "none") +
   theme_ofce()
